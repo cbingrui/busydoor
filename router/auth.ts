592d562da09f6cb36c6f6e1652a8142ae2553e45
Ray Chen <cbingrui@gmail.com>
2017-09-28 20:40:42 +0800

    test route

--- router/auth.ts
+++ router/auth.ts
@@ -4,15 +4,16 @@ import AuthCtrl from '../controllers/authController';
 export default function setRoutes(app) {
 
     const router = express.Router();
+    app.use('/api', router);
 
     const authCtrl = new AuthCtrl();
 
     // Cats
     router.route('/login').post(authCtrl.login);
-    router.use(authCtrl.authenticate);
-    router.route('/').get(function (req, res) {
+    router.get('/', authCtrl.authenticate, function (req, res) {
         res.status(201).json({ message: 'Welcome to the authenticated routes!' });
     });
+    router.use(authCtrl.authenticate);
     router.route('/users').get(authCtrl.getAll);
     router.route('/users/count').get(authCtrl.count);
     router.route('/user').post(authCtrl.insert);
@@ -20,7 +21,6 @@ export default function setRoutes(app) {
     router.route('/user/:id').put(authCtrl.update);
     router.route('/user/:id').delete(authCtrl.delete);
     // Apply the routes to our application with the prefix /api
-    app.use('/api', router);
 
 
 }
