44efba4682e347e5449f905f95bd7d6c732d4727
Ray Chen <cbingrui@gmail.com>
2017-11-07 17:12:50 +0800

    pin header

---
+++ src/app/modules/shared/directives/pin-header.directive.ts
@@ -0,0 +1,55 @@
+import { WindowRef } from './../window-ref';
+import { Directive, HostListener, ElementRef, Renderer, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appPinHeader]'
+})
+export class PinHeaderDirective implements OnInit {
+
+  lastScrollY = 0;
+  delta = 100;
+  // tslint:disable-next-line:no-input-rename
+  @Input('appPinHeader') pinHeaderStyleClass: string;
+
+  ngOnInit(): void {
+    console.log(this.pinHeaderStyleClass);
+    console.log('d');
+    if (this.pinHeaderStyleClass) {
+      this.el.nativeElement.classList.add(this.pinHeaderStyleClass);
+    } else {
+      this.renderer.setElementStyle(this.el.nativeElement, 'transition', 'top 0.3s ease-in-out');
+      this.renderer.setElementStyle(this.el.nativeElement, 'position', 'fixed');
+      this.renderer.setElementStyle(this.el.nativeElement, 'top', '0');
+      this.renderer.setElementStyle(this.el.nativeElement, 'right', '0');
+      this.renderer.setElementStyle(this.el.nativeElement, 'left', '0');
+      this.renderer.setElementStyle(this.el.nativeElement, 'z-index', '1030');
+    }
+  }
+
+  constructor(private el: ElementRef, private renderer: Renderer, private windRef: WindowRef) {
+
+  }
+
+  @HostListener('window:scroll', ['$event']) onScroll() {
+    const offsetY: number = this.windRef.nativeWindow.pageYOffset;
+    if (Math.abs(this.lastScrollY - offsetY) <= this.delta) {
+      return;
+    }
+
+    if (offsetY > this.lastScrollY) {
+      if (this.pinHeaderStyleClass) {
+        this.el.nativeElement.classList.remove(this.pinHeaderStyleClass);
+
+      } else {
+        this.renderer.setElementStyle(this.el.nativeElement, 'top', '-70px');
+      }
+    } else {
+      if (this.pinHeaderStyleClass) {
+        this.el.nativeElement.classList.add(this.pinHeaderStyleClass);
+      } else {
+        this.renderer.setElementStyle(this.el.nativeElement, 'top', '0');
+      }
+    }
+    this.lastScrollY = offsetY;
+  }
+}
