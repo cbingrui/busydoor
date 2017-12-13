72a1408236438cd674a277cc3a664109e0b39178
Ray Chen <cbingrui@gmail.com>
2017-12-13 23:27:30 +0800

    add process.env

--- server/config/database.ts
+++ server/config/database.ts
@@ -1,8 +1,9 @@
 const config = {
-    port: process.env.PORT || 3000,
-    MONGODB_URI: process.env.DOMAIN ? `mongodb://${process.env.DOMAIN}:27017/busydoor` : 'mongodb://localhost:27017/busydoor'
+    port: process.env.SERVER_PORT || 3000,
+    MONGODB_URI: process.env.DB_HOST ? `mongodb://${process.env.DB_HOST}:27017/busydoor` : 'mongodb://localhost:27017/busydoor'
     , SECRET_TOKEN: 'zookeeper'
-    , MONGODB_DOMAIN: process.env.DOMAIN || 'localhost:4200'
+    , MONGODB_DOMAIN: process.env.DB_HOST || 'localhost:4200'
+    , WEB_HOST: process.env.WEB_HOST || 'localhost:4200'
 };
 
 export default config;
--- server/server.ts
+++ server/server.ts
@@ -18,7 +18,7 @@ app.use(cookieParse());
 app.use(logger('dev'));
 app.use(helmet());
 app.use(cors({
-    origin: `http://${config.MONGODB_DOMAIN}`
+    origin: `http://${config.WEB_HOST}`
 }));
 
 // init
