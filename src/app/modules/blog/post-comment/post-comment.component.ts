f14ff7fc332ce3a327f8833dbc8ea6cfa04d9315
Ray Chen <cbingrui@gmail.com>
2017-12-13 23:13:47 +0800

    fix build error

--- src/app/modules/blog/post-comment/post-comment.component.ts
+++ src/app/modules/blog/post-comment/post-comment.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class PostCommentComponent implements OnInit {
 
   createdAt = new Date();
+  canModify: boolean;
   @Input() comment: Comment;
   constructor() { }
 
