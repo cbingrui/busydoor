d9bf78049c4a593baa6882048863ce30b050710f
Ray Chen <cbingrui@gmail.com>
2017-12-20 21:36:21 +0800

    deprecated delete login component

--- src/app/modules/account/account-routing.module.ts
+++ src/app/modules/account/account-routing.module.ts
@@ -1,25 +1,25 @@
 import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
 import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'register', component: SignInUpComponent
+    path: 'register',
+    component: SignInUpComponent
   },
   {
-    path: 'login', component: SignInUpComponent,
+    path: 'login',
+    component: SignInUpComponent
   },
   {
-    path: 'profile', component: ProfileComponent
+    path: 'profile',
+    component: ProfileComponent
   }
-
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AccountRoutingModule { }
+export class AccountRoutingModule {}
--- src/app/modules/account/account.module.ts
+++ src/app/modules/account/account.module.ts
@@ -1,20 +1,12 @@
-
-
 import { SharedModule } from './../shared/shared.module';
 import { AccountRoutingModule } from './account-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignInUpComponent } from './sign-in-up/sign-in-up.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    SharedModule,
-    AccountRoutingModule
-  ],
-  declarations: [RegisterComponent, LoginComponent, ProfileComponent, SignInUpComponent]
+  imports: [CommonModule, SharedModule, AccountRoutingModule],
+  declarations: [ProfileComponent, SignInUpComponent]
 })
-export class AccountModule { }
+export class AccountModule {}
