fe34aed2f98ce4a9a5fce1b7d57a997bfe036e2f
Ray Chen <cbingrui@gmail.com>
2017-12-25 08:58:12 +0800

    add mongoose  config option

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,16 @@ class ServerApp {
   }
 
   private initDB() {
-    mongoose.connect(config.MONGODB_URI);
+    const options = {
+      useMongoClient: true,
+      autoIndex: false, // Don't build indexes
+      reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+      reconnectInterval: 500, // Reconnect every 500ms
+      poolSize: 10, // Maintain up to 10 socket connections
+      // If not connected, return errors immediately rather than waiting for reconnect
+      bufferMaxEntries: 0
+    };
+    mongoose.connect(config.MONGODB_URI, options);
   }
 
   private initExpress() {
